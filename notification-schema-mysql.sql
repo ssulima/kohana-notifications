4f82a69dab7ea5e63af8dfac65d4c3c1f33d9fb1
Roomcays <roomcays@wp.pl>
2015-04-22 16:11:03 +0200

    SQL schemas update.

--- notification-schema-mysql.sql
+++ notification-schema-mysql.sql
@@ -1,9 +1,16 @@
 CREATE TABLE IF NOT EXISTS `notifications` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-	`type` varchar(20) NOT NULL,
-  `description` varchar(200) NOT NULL,
-	`created` int(10) NOT NULL,
-	`processed` int(10) unsigned DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `type` (`type`)
+    `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+    `type` varchar(20) NOT NULL,
+    `description` varchar(200) NOT NULL,
+    `created` int(10) NOT NULL,
+    `processed` int(10) unsigned DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `type` (`type`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+
+CREATE TABLE IF NOT EXISTS `notifications_mail_queue` (
+    `id` int(11) unsigned NOT NULL,
+    `notification_id` int(11) unsigned NOT NULL,
+    `recipient` varchar(200) NOT NULL,
+    `processed` int(10) unsigned DEFAULT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
--- notification-schema-postgresql.sql
+++ notification-schema-postgresql.sql
@@ -1,11 +1,20 @@
 CREATE TABLE notifications
 (
-  id serial,
-	"type" varchar(20) NOT NULL,
-	description varchar(200) NOT NULL,
-  created integer NOT NULL,
-  processed integer NULL,
-	CONSTRAINT notifications_id_pkey PRIMARY KEY (id)
+    id serial,
+    "type" varchar(20) NOT NULL,
+    description varchar(200) NOT NULL,
+    created integer NOT NULL,
+    processed integer NULL,
+    CONSTRAINT notifications_id_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE notifications_mail_queue
+(
+    id serial,
+    notification_id int(11) NOT NULL,
+    recipient varchar(200) NOT NULL,
+    processed integer NULL,
+    CONSTRAINT notifications_mail_queue_id_pkey PRIMARY KEY (id)
 );
 
 CREATE INDEX type_idx ON notifications (type);
